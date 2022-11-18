f69c4a6589d967997763f9bad93bd29af1485f4d
OOJJHHLL <114508503+OOJJHHLL@users.noreply.github.com>
2022-11-18 11:22:53 +0100

    Updated the code

--- CAN_and_IMU/CAN_and_IMU.ino
+++ CAN_and_IMU/CAN_and_IMU.ino
@@ -1,19 +1,5 @@
-/* Teensy CAN-Bus with OLED 128x64 */
-
-// -------------------------------------------------------------
-// CANtest for Teensy 3.6 dual CAN bus
-// by Collin Kidder, Based on CANTest by Pawelsky (based on CANtest by teachop)
-//
-// Both buses are left at default 250k speed and the second bus sends frames to the first
-// to do this properly you should have the two buses linked together. This sketch
-// also assumes that you need to set enable pins active. Comment out if not using
-// enable pins or set them to your correct pins.
-//
-// This sketch tests both buses as well as interrupt driven Rx and Tx. There are only
-// two Tx buffers by default so sending 5 at a time forces the interrupt driven system
-// to buffer the final three and send them via interrupts. All the while all Rx frames
-// are internally saved to a software buffer by the interrupt handler.
-//
+// Sende IMU-verdier over CAN-bus 
+// Koden basserer seg på  CANtest for Teensy 3.6 dual CAN bus av Collin Kidder fra FlexCAN library master biblioteket
 
 #include <FlexCAN.h>
 #include <SPI.h>
@@ -22,7 +8,7 @@
 #include <Adafruit_SSD1306.h>
 #include <MPU9250_WE.h>
 
-#define MPU9250_ADDR 0x68 // endret fra 0x40
+#define MPU9250_ADDR 0x68 
 
 
 MPU9250_WE myMPU9250 = MPU9250_WE(MPU9250_ADDR);
@@ -33,10 +19,6 @@ MPU9250_WE myMPU9250 = MPU9250_WE(MPU9250_ADDR);
 #define OLED_RESET  5
 Adafruit_SSD1306 display(OLED_DC, OLED_RESET, OLED_CS);
 
-
-#ifndef __MK66FX1M0__
-  #error "Teensy 3.6 with dual CAN bus is required to run this example"
-#endif
 
 static CAN_message_t msg;
 CAN_message_t inMsg;
@@ -61,48 +43,10 @@ static void hexDump(uint8_t dumpLen, uint8_t *bytePtr)
 int rCount = 1;
 
 
-
-/*
-void IMU_setup()
-{
-Serial.begin(115200);
-  Wire.begin();
-  if(!myMPU9250.init()){
-    Serial.println("MPU9250 does not respond");
-  }
-  else{
-    Serial.println("MPU9250 is connected");
-  }
-  if(!myMPU9250.initMagnetometer()){
-    Serial.println("Magnetometer does not respond");
-  }
-  else{
-    Serial.println("Magnetometer is connected");
-  }
-
-  Serial.println("Position you MPU9250 flat and don't move it - calibrating...");
-  delay(1000);
-  myMPU9250.autoOffsets();
-  Serial.println("Done!");
-
-  
-  myMPU9250.enableGyrDLPF();
-  myMPU9250.setGyrDLPF(MPU9250_DLPF_6);
-  myMPU9250.setSampleRateDivider(5);
-  myMPU9250.setGyrRange(MPU9250_GYRO_RANGE_250);
-  myMPU9250.setAccRange(MPU9250_ACC_RANGE_2G);
-  myMPU9250.enableAccDLPF(true);
-  myMPU9250.setAccDLPF(MPU9250_DLPF_6);
-  myMPU9250.setMagOpMode(AK8963_CONT_MODE_100HZ);
-  delay(200);
-}
-*/
-
 void setup() 
 {
 Can0.begin(250000);  
 
-  //if using enable pins on a transceiver they need to be set on
   // Sjekk at du bruker riktig pin
   pinMode(2, OUTPUT); //can0
 
@@ -152,7 +96,7 @@ Can0.begin(250000);
 
 void set_display()
 {  
-  display.clearDisplay(); // Clears the display
+  display.clearDisplay(); 
   display.drawRoundRect(1,1,127,63,4,WHITE);
   display.setTextSize(0);
   display.setTextColor(WHITE);
@@ -197,19 +141,20 @@ void loop()
 while (Can0.available()) 
   {
 
-    set_display();
-    Can0.read(inMsg); // Leser fra transmit i PCAN-view? 
+    set_display(); // Setter opp displayet
+
+    Can0.read(inMsg); // Leser melding sendt utenifra (fra PCAN-view)
     hexDump(8, inMsg.buf);
 
     display.setCursor(110,30);
     display.println(rCount);
     display.setCursor(120,40);
-    display.println(inMsg.id,HEX); //Converterer fra hex til int hvis jeg ikke skriver ,hex
+    display.println(inMsg.id,HEX); // Skriver HEX for å unngå at IDen konverteres til int
 
   }  
   if (inMsg.id == 33) //  0x21 = 33
     {
-      rCount = 1;
+      rCount = 1; 
       set_display();
       display.setCursor(110,30);
       display.println(rCount);
@@ -218,18 +163,19 @@ while (Can0.available())
       display.setCursor(95, 52);
       display.println("m/s^2");
 
-      msg.buf[0] = gValue.z * 9.81;
-      msg.buf[1] = gValue.y * 9.81;
-      msg.buf[2] = gValue.x * 9.81;
+    // Meldingene som sendes til PCAN-view
+      msg.buf[0] = gValue.z;
+      msg.buf[1] = gValue.y;
+      msg.buf[2] = gValue.x;
 
-      Can0.write(msg);
+      Can0.write(msg); // Meldingen sendes
+
       display.setCursor(69,52);
-      display.println(gValue.z*9.81);
+      display.println(gValue.z*9.81); // Printer g-verdien på OLED-skjermen
 
-      //rCount++;
       display.display();
       delay(20);
       display.clearDisplay();
       }
-  delay(1000);
+  delay(1000); // Meldingen sendes med en rate på 1Hz
 }
